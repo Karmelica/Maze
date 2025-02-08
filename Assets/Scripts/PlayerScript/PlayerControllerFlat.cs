181f76177884d86e78e80b4b4e57a48526b58915
Damian Niedziółka <95479392+Karmelica@users.noreply.github.com>
2025-02-08 05:24:11 +0100

    2

---
+++ Assets/Scripts/PlayerScript/PlayerControllerFlat.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerFlat : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    private bool isGrounded;
+    private Rigidbody2D rb;
+    //private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        //animator.SetFloat("Speed", Mathf.Abs(moveInput));
+        spriteRenderer.flipX = moveInput < 0;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
